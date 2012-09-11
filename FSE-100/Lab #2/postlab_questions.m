f5dd5b77f9d6527801b8157f718e56ff9bbe2bfa
Anthony Kosednar <anthony.kosednar@gmail.com>
2012-09-11 13:50:47 -0700

    Added "good" documentation
    …grumble grumble grumble…..

--- FSE-100/Lab #2/postlab_questions.m
+++ FSE-100/Lab #2/postlab_questions.m
@@ -1,8 +1,17 @@
+% Description: FSE 100 - Lab #2 - MATLAB Practice Script
+% Author: Anthony Kosednar
+% Date: 09/11/2012
+
+% Define Variables
 x = linspace(0,2*pi,100);
 g = x;
 y = sin(2*x);
 h = 2*cos(2*z);
+
+% Do Our Plotting
 plot(x,y,'c:+',g,h,'g--*')
+
+% Create our labels
 title('Plot of f(x)=sin(2x) and its derivative.')
 xlabel('X')
 ylabel('Y')
